ca61ccbf6567cdf5add5f6554dba4c1139910d52
sandesh9988 <sandesh.lastdoor@gmail.com>
2023-05-18 14:09:52 +0545

    reactjs to add contact

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,33 @@
-import React from "react";
-import './App.css';
+import React, { useState , useEffect } from "react"; //useState is react hooks for functional component and 
+import './App.css';                               // useEffect is another react hook which helps to render the component when the value changes and also as a local storage
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]); //array is empty cause contacts have empty value in initial
+ 
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]); //to update the state and display on ui
+  };
 
-  const contacts = [
-    {
-      id: "1",
-      name: "Ram",
-      email: "ram.lastdoor@gmail.com",
-    },
-    {
-      id: "1",
-      name: "Shyam",
-      email: "shyam.lastdoor@gmail.com",
-    },
-  ]
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)); // stringify converts a JavaScript value to a JSON string
+ if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts)); // stringify converts a JavaScript value to a JSON string
+  }, [contacts]);
+
   return (
-   <div className="ui container">
-    <Header/>
-    <AddContact/> 
-    <ContactList contacts={contacts}/>
-   </div>
+    <div className="ui container">
+      <Header />
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
+    </div>
   );
 }
 
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import user from "../images/user.png";
 
 const ContactCard = (props) => {
-    const { id, name, email } = props.contact;
+    const { name, email } = props.contact;
     return (
         <div className="item" style={{ display: "flex", alignItems: "center", justifyContent: "space-between", padding: "20px" }}>
             <div className="content" style={{ display: "flex", alignItems: "center", justifyContent: "space-between", gap: "10px" }}>
